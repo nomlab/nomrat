4f05b035b7a2620ea9ac3cfedc952f1b89da054a
Yoshinari Nomura <nom@quickhack.net>
2014-06-22 12:15:45 +0900

    Use new Scrap and Reporter

--- lib/nomnichi_bot.rb
+++ lib/nomnichi_bot.rb
@@ -1,11 +1,5 @@
 require "nomnichi_bot/version"
+require 'nomnichi_bot/scrap'
+require 'nomnichi_bot/reporter'
+require 'nomnichi_bot/tweet_test'
 require 'nomnichi_bot/tweet_sender'
-require 'nomnichi_bot/mail_sender'
-require 'nomnichi_bot/nomnichi_getter'
-require 'nomnichi_bot/kinro_getter'
-require 'nomnichi_bot/redmine_getter'
-require 'nomnichi_bot/debiansec_getter'
-
-module NomnichiBot
-  # Your code goes here...
-end

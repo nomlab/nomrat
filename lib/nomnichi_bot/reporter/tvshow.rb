9787e814c1071d89f7e33d3b65473b5c2a979554
Yoshinari Nomura <nom@quickhack.net>
2014-06-22 12:23:48 +0900

    Not to show future TV-show titles

--- lib/nomnichi_bot/reporter/tvshow.rb
+++ lib/nomnichi_bot/reporter/tvshow.rb
@@ -5,7 +5,7 @@ module NomnichiBot
 
       def report
         # return nil unless program = NomnichiBot::Scrap.kinro.today
-        return nil unless program = NomnichiBot::Scrap.kinro.last
+        return nil unless program = NomnichiBot::Scrap.kinro.today
         return "本日(#{program.date})の金曜ロードショーは" +
           "「#{program.title}」です．皆様早く帰りましょう．"
       end

8f57d922bc1090d4dc12ce4b3b60ed770eca4296
Yoshinari Nomura <nom@quickhack.net>
2014-09-01 18:02:11 +0900

    Add Slack support

---
+++ lib/nomnichi_bot/slack_sender.rb
@@ -0,0 +1,32 @@
+require "net/https"
+require "uri"
+require "json"
+
+module NomnichiBot
+  class SlackSender
+    def initialize
+      config = Config.load(:slack)
+
+      @team     = config["team"]
+      @token    = config["token"]
+      @channel  = config["channel"]
+      @username = config["username"]
+      @post_uri = "https://#{@team}.slack.com/services/hooks/incoming-webhook?token=#{@token}"
+    end
+
+    def send_message(string)
+      uri = URI.parse(@post_uri)
+      res = nil
+      json = {channel: @channel, username: @username, text: string}.to_json
+      request = "payload=" + json
+
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        res = http.post(uri.request_uri, request)
+      end
+
+      puts res.body if $NOMNICHI_BOT_DEBUG
+      return res
+    end
+  end # class SlackSender
+end # module NomnichiBot

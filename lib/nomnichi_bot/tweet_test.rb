45830ff18511a920c385e2462e3784443cf5350d
Yoshinari Nomura <nom@quickhack.net>
2014-09-01 18:00:44 +0900

    Make test print clear

--- lib/nomnichi_bot/tweet_test.rb
+++ lib/nomnichi_bot/tweet_test.rb
@@ -1,7 +1,7 @@
 module NomnichiBot
   class TweetTest
     def send_message(string)
-      puts "TweetTest: #{string}"
+      puts "TweetTest: '#{string}'"
       return string
     end
   end # class TweetTest

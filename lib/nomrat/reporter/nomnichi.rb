99acf7677f264cd84a115ca49e10c8b205268e50
Yoshinari Nomura <nom@nomlab.org>
2015-04-22 19:15:07 +0900

    Introduce fair assignment of nomnichi-author

--- lib/nomrat/reporter/nomnichi.rb
+++ lib/nomrat/reporter/nomnichi.rb
@@ -4,11 +4,17 @@ module Nomrat
     class Nomnichi
 
       def report
-        return nil unless article = Scrap.open(:nomnichi).last
-        msg = "次のノムニチ担当は #{next_author(article.author)} さんです．"
+        return nil unless articles = Scrap.open(:nomnichi).articles
 
-        if (ago = (Date.today - article.date).to_i) >= 7
-          msg << "最後の記事(#{ago}日前)から1週間以上経ってますよ？"
+        author = next_author(articles)
+        last_article = last_article(author, articles)
+
+        msg = "次のノムニチ担当は #{author} さんです．"
+
+        if !last_article
+          msg << "ここ最近，あなたの記事を見ていませんよ？"
+        elsif (ago = (Date.today - last_article.date).to_i) >= 7
+          msg << "最後のあなたの記事(#{ago}日前)から随分経ってますよ？"
         end
 
         return msg
@@ -16,24 +22,30 @@ module Nomrat
 
       private
 
-      def next_author(author)
-        author_order = {
-          "nom"       => "okada",
-          "okada"     => "kitagaki",
-          "kitagaki"  => "masuda-y",
-          "masuda-y"  => "ikeda-y",
-          "ikeda-y"   => "ichikawa",
-          "ichikawa"  => "kobayashi",
-          "kobayashi" => "nakamura",
-          "nakamura"  => "fujita",
-          "fujita"    => "emi",
-          "emi"       => "suetake",
-          "suetake"   => "sugi",
-          "sugi"      => "date",
-          "date"      => "yoshida-h",
-          "yoshida-h" => "nom"
-        }
-        author_order[author] || "nom"
+      def authors
+        return %w(
+           nom
+           okada kitagaki masuda-y
+           ikeda-y ichikawa kobayashi nakamura fujita
+           emi suetake sugi date yoshida-h
+        )
+      end
+
+      def next_author(articles)
+        candidates = authors
+
+        articles.sort {|a,b| b.date <=> a.date}.each do |article|
+          break if candidates.length < 2
+          candidates.delete(article.author)
+        end
+        return candidates.shift
+      end
+
+      def last_article(author, articles)
+        articles.sort {|a,b| b.date <=> a.date}.each do |article|
+          return article if article.author == author
+        end
+        return nil
       end
 
     end # class Nomnichi

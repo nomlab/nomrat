f4a5c54f9ee5d5c2f5ad950998e3aa5452e20809
Yoshinari Nomura <nom@nomlab.org>
2015-07-31 11:10:34 +0900

    List multiple candidates for nomnichi authors

--- lib/nomrat/reporter/nomnichi.rb
+++ lib/nomrat/reporter/nomnichi.rb
@@ -6,15 +6,20 @@ module Nomrat
       def report
         return nil unless articles = Scrap.open(:nomnichi).articles
 
-        author = next_author(articles)
-        last_article = last_article(author, articles)
+        msg = "ノムニチを書いてくれませんか．"
 
-        msg = "次のノムニチ担当は #{author} さんです．"
+        next_authors(articles, 2).each do |author|
+          last_article = last_article(author, articles)
 
-        if !last_article
-          msg << "ここ最近，あなたの記事を見ていませんよ？"
-        elsif (ago = (Date.today - last_article.date).to_i) >= 7
-          msg << "最後のあなたの記事(#{ago}日前)から随分経ってますよ？"
+          msg << "#{author} さん，"
+
+          if !last_article
+            msg << "たなたの記事を随分見ていません．"
+          elsif (ago = (Date.today - last_article.date).to_i) >= 7
+            msg << "最後の記事(#{ago}日前)から随分経ってますよ．"
+          end
+
+          msg << "\n"
         end
 
         return msg
@@ -31,14 +36,25 @@ module Nomrat
         )
       end
 
-      def next_author(articles)
-        candidates = authors
+      def sort_authors_by_article_date(authors, articles)
+        authors.sort do |author_a, author_b|
+          a = last_article(author_a, articles)
+          b = last_article(author_b, articles)
 
-        articles.sort {|a,b| b.date <=> a.date}.each do |article|
-          break if candidates.length < 2
-          candidates.delete(article.author)
+          if a && b
+            a.date <=> b.date
+          elsif !a && b
+            -1
+          elsif a && !b
+            1
+          else # !a && !b
+            0
+          end
         end
-        return candidates.shift
+      end
+
+      def next_authors(articles, max_authors = 1)
+        sort_authors_by_article_date(authors, articles).take(max_authors)
       end
 
       def last_article(author, articles)
